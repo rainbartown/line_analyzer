fe00beae39ca2daa0b92de6063edabac569d4219
Gandats <47085394+Gandats@users.noreply.github.com>
2019-07-12 18:07:13 +0900

    add table page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from '@/components/HomePage';
+import TablePage from '@/components/TablePage';
 
 Vue.use(Router);
 
-const TablePage = { template: '<div>TablePage</div>' };
 const ChartPage = { template: '<div>ChartPage</div>' };
 
 export default new Router({
