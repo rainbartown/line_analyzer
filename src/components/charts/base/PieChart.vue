04708cd49d315d415dddc16930e98d289c7a7de3
Gandats <47085394+Gandats@users.noreply.github.com>
2019-07-12 21:01:30 +0900

    add pie chart

---
+++ src/components/charts/base/PieChart.vue
@@ -0,0 +1,30 @@
+<script>
+import { Pie, mixins } from 'vue-chartjs';
+
+export default {
+  name: 'PieChart',
+  extends: Pie,
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    chartData() {
+      this.renderChart(this.chartData, this.options);
+    },
+    options() {
+      this.renderChart(this.chartData, this.options);
+    },
+  },
+};
+</script>
