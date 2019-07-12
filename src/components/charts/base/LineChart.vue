f2683365339e31475a6dff76c056b83fbb461047
Gandats <47085394+Gandats@users.noreply.github.com>
2019-07-12 21:01:47 +0900

    add line chart

---
+++ src/components/charts/base/LineChart.vue
@@ -0,0 +1,30 @@
+<script>
+import { Line, mixins } from 'vue-chartjs';
+
+export default {
+  name: 'LineChart',
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    data() {
+      this.renderChart(this.chartData, this.options);
+    },
+    options() {
+      this.renderChart(this.chartData, this.options);
+    },
+  },
+};
+</script>
