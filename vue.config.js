fa71d8e9b3e97084576229d46edd779c73ceedf7
Gandats <47085394+Gandats@users.noreply.github.com>
2019-08-14 00:57:33 +0900

    change production building config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ module.exports = {
   // for GitHub Pages
   outputDir: 'docs',
   publicPath: './',
+  filenameHashing: false,
+  productionSourceMap: false,
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.resolve.alias
+        .set('chart.js', 'chart.js/dist/Chart.bundle.min.js');
+    }
+  },
 };
