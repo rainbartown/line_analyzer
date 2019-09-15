9b48a3199a569126cc710316d00ccb2ac1253146
Gandats <47085394+Gandats@users.noreply.github.com>
2019-09-16 03:31:29 +0900

    [add] iOSのPWA更新のための設定を追加

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,13 @@ module.exports = {
   publicPath: './',
   filenameHashing: false,
   productionSourceMap: false,
+  pwa: {
+    workboxOptions: {
+      // iOSのキャッシュがアップデートされない問題の対策
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+  },
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.resolve.alias
