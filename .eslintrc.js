5012fb0dc6610ca5dddcc1d66da9887584dd2d11
Gandats <47085394+Gandats@users.noreply.github.com>
2020-06-06 22:06:28 +0900

    Jestを導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,20 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }]  // for TypeScript
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }], // for TypeScript
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
 };
