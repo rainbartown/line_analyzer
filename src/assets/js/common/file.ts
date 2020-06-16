2aed38403bb27f0408293593a66efc08c83512ff
Gandats <47085394+Gandats@users.noreply.github.com>
2020-06-16 23:57:09 +0900

    テキストファイルを読む機能を修正

---
+++ src/assets/js/common/file.ts
@@ -0,0 +1,32 @@
+/**
+ * ファイルに関するモジュール
+ */
+
+
+/**
+ * テキストファイルを読み、中身のテキストを返す関数
+ * @param file テキストファイル
+ * @returns 中身のテキスト
+ */
+const readTextFile = (file: File): Promise<string> => (
+  new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const { result } = reader;
+      if (typeof result === 'string') resolve(result);
+      reject(new Error('ファイル読み込みでエラーが発生しました'));
+    });
+
+    reader.addEventListener('error', () => {
+      reject(new Error('ファイル読み込みでエラーが発生しました'));
+    });
+
+    reader.readAsText(file);
+  })
+);
+
+
+export {
+  readTextFile, // eslint-disable-line import/prefer-default-export
+};
